79088a62380d3cdbce34820ac31a8d6fed14bf52
Konstantin3688 <okatovka3688@gmail.com>
2022-07-07 17:30:44 +0300

    add if else

---
+++ Example005_ConfigurationIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "александра")
+{
+    Console.WriteLine("Вот это ДА, сама Александра явила свой лик!");
+}
+else
+{
+    Console.Write("Даров, ");
+    Console.WriteLine(username);
+}
